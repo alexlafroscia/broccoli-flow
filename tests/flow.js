c761112d80c4fb771e53dc51a33ac04b0b61f3c7
Alex LaFroscia <alex@lafroscia.com>
2016-07-16 11:15:44 -0700

    Code cleanup

--- tests/flow.js
+++ tests/flow.js
@@ -1,18 +1,28 @@
 var resolve = require('path').resolve;
 
 var expect = require('chai').expect;
+
 var flow = require('../lib/flow');
+var checkFile = flow.checkFile;
+var readFile = flow.readFile;
+var checkFileContents = flow.checkFileContents;
+
+var FlowCheckFailure = require('../lib/error').FlowCheckFailure;
 
 describe('flow module', function() {
-  it('should export the `readFile` function', function() {
-    expect(flow.readFile).to.be.instanceof(Function);
+  describe('readFile', function() {
+    it('should export the function', function() {
+      expect(readFile).to.be.instanceof(Function);
+    });
   });
 
-  it('should export the `checkFileContents` function', function() {
-    expect(flow.checkFileContents).to.be.instanceof(Function);
+  describe('checkFileContents', function() {
+    it('should export the function', function() {
+      expect(flow.checkFileContents).to.be.instanceof(Function);
+    });
   });
 
-  describe('file checking function', function() {
+  describe('checkFile', function() {
     describe('function parameters', function() {
       [
         { type: 'a number', value: 0 }, { type: 'undefined', value: undefined },
@@ -20,7 +30,7 @@ describe('flow module', function() {
         { type: 'an invalid path', value: 'foo.js' }
       ].forEach(function(data) {
         it('should reject when ' + data.type + ' is provided', function() {
-          return flow(data.value)
+          return checkFile(data.value)
             .catch(function(error) {
               expect(error).instanceof(Error);
             })
@@ -29,17 +39,17 @@ describe('flow module', function() {
             });
         });
       });
+    });
 
-      it('should resolve if the file passes the check', function() {
-        return flow(resolve(__dirname, './input/valid.js'));
-      });
+    it('should resolve if the file passes the check', function() {
+      return checkFile(resolve(__dirname, './input/valid.js'));
+    });
 
-      it('should reject if the file did not pass a check', function() {
-        return flow(resolve(__dirname, './input/invalid.js'))
-          .catch(function(output) {
-            expect(output).to.be.instanceof(Error);
-          });
-      });
+    it('should reject if the file did not pass a check', function() {
+      return checkFile(resolve(__dirname, './input/invalid.js'))
+        .catch(function(error) {
+          expect(error).to.be.instanceof(FlowCheckFailure);
+        });
     });
   });
 });
